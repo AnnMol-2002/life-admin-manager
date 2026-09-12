63aa9d0d85bdfdf6ce1b427bf22463e03e4f503d
AnnMol-2002 <annmolanto2002@gmail.com>
2026-09-13 00:02:15 +0530

    backend initial setup with spring initializr and db configuration setup in the applications.properties file

---
+++ backend/src/test/java/com/lifeadminmanager/life_admin_manager_backend/LifeAdminManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifeadminmanager.life_admin_manager_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeAdminManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
